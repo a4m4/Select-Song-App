7555d272743b35bb90b560869ed9a9681ce9b13e
Ali Mehdi <salimehdi144@gmail.com>
2019-05-25 18:08:28 +0500

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import SongList from './SongList';
 import { selectSong } from '../actions';
+import SongDetail from './SongDetail';
+
+
 const App = () => {
 
-    return <div> hey hello</div>
+    return (
+        <div className="ui container grid">
+            <div className="ui row">
+                <div className="column eight wide">
+                    <SongList />
+                </div> 
+                <div className="column eight wide">
+                    <SongDetail />
+                </div>
+            </div>
+        </div>
+        );
 };
 
 export default App;
